a2e8c71ac9a689c9eca9cba3dce24b87290138d9
Chae Seungwoo <sduck@astrsk.ai>
2025-07-25 15:15:30 +0900

    feat: get model id manually for oai comp

--- apps/pwa/src/modules/api/usecases/list-api-model/list-openai-compatible-model-strategy.ts
+++ apps/pwa/src/modules/api/usecases/list-api-model/list-openai-compatible-model-strategy.ts
@@ -1,38 +1,24 @@
 import { Result } from "@/shared/core/result";
-import { OpenAIComptableEndpoint } from "@/shared/endpoints";
-import { HttpClient } from "@/shared/infra";
 import { formatFail } from "@/shared/utils/error-utils";
 
 import { ApiConnection, ApiModel } from "@/modules/api/domain";
 import { ListApiModelStrategy } from "@/modules/api/usecases/list-api-model/list-api-model-strategy";
 
 export class ListOpenaiCompatibleModelStrategy implements ListApiModelStrategy {
-  constructor(private httpClient: HttpClient) {}
+  constructor() {}
 
   async listApiModel(
     apiConnection: ApiConnection,
   ): Promise<Result<ApiModel[]>> {
     try {
       if (!apiConnection.props.apiKey) {
-        throw new Error("API key is missing for OpenAI connection");
+        throw new Error("API key is missing for OpenAI compatible connection");
       }
 
-      const endpoint = new OpenAIComptableEndpoint(
-        this.httpClient,
-        apiConnection.props.apiKey,
-        apiConnection.props.baseUrl,
-      );
-
-      let modelList = await endpoint.getAvailableModelList();
-
-      if (apiConnection.modelUrls) {
-        modelList = [
-          ...modelList,
-          ...apiConnection.modelUrls.map((url) =>
-            ApiModel.create({ id: url, name: url }).getValue(),
-          ),
-        ];
-      }
+      const modelList =
+        apiConnection.modelUrls?.map((url) =>
+          ApiModel.create({ id: url, name: url }).getValue(),
+        ) ?? [];
 
       return Result.ok(modelList);
     } catch (error) {
