15619b49ab8be72dbf7e0081381b5f31234a4f05
Chae Seungwoo <sduck@astrsk.ai>
2025-09-05 10:55:21 +0900

    feat: connect convex and clerk

--- apps/pwa/src/main.tsx
+++ apps/pwa/src/main.tsx
@@ -2,12 +2,18 @@ import { initServices } from "@/app/services/init-services.ts";
 import { initStores } from "@/app/stores/init-stores.ts";
 import { Loading } from "@/components-v2/loading.tsx";
 import { migrate } from "@/db/migrate.ts";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { Buffer } from "buffer";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { enableMapSet } from "immer";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+// Convex
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 // Enable Map/Set support in Immer before anything else
 enableMapSet();
@@ -42,7 +48,13 @@ async function initializeApp() {
   // Render app
   root.render(
     <StrictMode>
-      <App />
+      <ClerkProvider
+        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      >
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <App />
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
     </StrictMode>,
   );
 }
