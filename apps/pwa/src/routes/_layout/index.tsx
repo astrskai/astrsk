e75a155bf5a677c0979586f8b9c52a85df72d208
dwbang <dbang@astrsk.ai>
2025-11-26 16:54:05 +0900

    Remove deprecated settings sample page and update routing to use HomePage instead

--- apps/pwa/src/routes/_layout/index.tsx
+++ apps/pwa/src/routes/_layout/index.tsx
@@ -1,7 +1,6 @@
 import { HomePage } from "@/pages/home";
 import { createFileRoute } from "@tanstack/react-router";
-import SettingsSamplePage from "@/pages/settings-sample";
 
 export const Route = createFileRoute("/_layout/")({
-  component: SettingsSamplePage,
+  component: HomePage,
 });
