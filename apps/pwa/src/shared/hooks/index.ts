2e8512464088d65631067c423e6d0914cabf39e5
Younduk Nam <nyd@astrsk.ai>
2025-12-19 14:17:53 +0900

    Fixe missing import

--- apps/pwa/src/shared/hooks/index.ts
+++ apps/pwa/src/shared/hooks/index.ts
@@ -3,5 +3,4 @@ export { useDeviceType, useIsMobile } from "./use-device-type";
 export { useIsTouchDevice } from "./use-is-touch-device";
 
 // Other commonly used hooks
-export { useMobile } from "./use-mobile";
 export { usePwa } from "./use-pwa";
